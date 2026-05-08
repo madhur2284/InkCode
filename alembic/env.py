1fa6b63e30e502f6accd328c524f52df69a8dc85
Madhur <debugginglife28@gmail.com>
2026-05-08 22:07:05 +0530

    adding DATABASE URL in config

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,12 @@ import models.db_models
 
 config = context.config
 
+config.set_main_option(
+    "sqlalchemy.url",
+    settings().DATABASE_URL
+)
+
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
