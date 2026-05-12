9961b645e155bd53bfecf934c61203ea3571d4b3
Madhur <debugginglife28@gmail.com>
2026-05-12 19:06:06 +0530

    add Cors Middleware and In Memory Middleware

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cloudinary_config
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
 from contextlib import asynccontextmanager
 from routers import auth, post, comment, tag, post_reaction, comment_reaction, bookmark, follow, search, admin_stats
 from database import engine
 from fastapi.middleware.cors import CORSMiddleware
+from collections import defaultdict
+import time
+from fastapi.responses import JSONResponse
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +16,36 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+rate_limit = 60
+windows_start = 60
+requests_count: dict = defaultdict(list)
+
+@app.middleware("http")
+async def rate_limit_middleware(request: Request, call_next):
+    ip = request.client.host
+    now = time.time()
+    windows_seconds = now-windows_start
+    requests_count[ip] = [ts for ts in requests_count[ip] if ts > windows_seconds]
+    
+    if len(requests_count[ip]) > rate_limit:
+        return JSONResponse(status_code=status.HTTP_429_TOO_MANY_REQUESTS, content={"detail": "Too many Requests"})
+    
+    requests_count[ip].append(now)
+    return await call_next(request)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router)
 app.include_router(post.router)
